b267f60f316d158cf903ba9c026a080fe7822908
mriskiaziz <mriski.119140160@student.itera.ac.id>
2023-08-26 20:03:34 +0700

    perbaikan

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -59,7 +59,8 @@ export default function LoginForm() {
           )}
 
           <Link className="text-sm mt-3 text-right" href={"/register"}>
-            Don't have an account? <span className="underline">Register</span>
+            {`Don't have an account?`}{" "}
+            <span className="underline">Register</span>
           </Link>
         </form>
       </div>
